3c907b92028b9adb491c34735f8f888b10719207
josh <josh@email.com>
2023-03-26 13:25:08 -0600

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module example/influx_db_client
+
+go 1.18
+
+require (
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.12.2 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+func main() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading environment variables: ", err)
+	}
+
+	influxUrl := fmt.Sprintf("https://%s:8086", os.Getenv("INFLUXDB_SERVER_IP"))
+	influxToken := os.Getenv("INFLUXDB_API_TOKEN")
+	influxBucket := os.Getenv("INFLUXDB_BUCKET")
+	influxOrg := os.Getenv("INFLUXDB_ORG")
+
+	// Create a new client using an InfluxDB server base URL and an authentication token
+
+	// This is the default client instantiation.  However, due to server using
+	// a self-signed TLS certificate without using SANs, this throws an error
+	// client := influxdb2.NewClient(influxUrl, influxToken)
+
+	// To skip certificate verification, create the client like this
+
+	// Create a new TLS configuration with certificate verification disabled
+	// This is not recommended though.  Only use for testing until a new
+	// TLS certificate can be created with SANs
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	client := influxdb2.NewClientWithOptions(influxUrl, influxToken, influxdb2.DefaultOptions().SetTLSConfig(tlsConfig))
+
+	// Use blocking write client for writes to desired bucket
+	writeAPI := client.WriteAPIBlocking(influxOrg, influxBucket)
+
+	// Create point using full params constructor
+	// p := influxdb2.NewPoint("GoTestData",
+	// 	map[string]string{"unit": "temperature"},
+	// 	map[string]interface{}{"avg": 24.5, "max": 45.0},
+	// 	time.Now())
+
+	// // write point immediately
+	// writeAPI.WritePoint(context.Background(), p)
+
+	// Create point using fluent style
+	p := influxdb2.NewPointWithMeasurement("GoTestData").
+		AddTag("unit", "temperature").
+		AddField("avg", 23.2).
+		AddField("max", 45.0).
+		SetTime(time.Now())
+
+	err = writeAPI.WritePoint(context.Background(), p)
+	if err != nil {
+		panic(err)
+	}
+	// Or write directly line protocol
+	// line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
+	// err = writeAPI.WriteRecord(context.Background(), line)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// Get query client
+	// queryAPI := client.QueryAPI("my-org")
+	// // Get parser flux query result
+	// result, err := queryAPI.Query(context.Background(), `from(bucket:"my-bucket")|> range(start: -1h) |> filter(fn: (r) => r._measurement == "stat")`)
+	// if err == nil {
+	// 	// Use Next() to iterate over query result lines
+	// 	for result.Next() {
+	// 		// Observe when there is new grouping key producing new table
+	// 		if result.TableChanged() {
+	// 			fmt.Printf("table: %s\n", result.TableMetadata().String())
+	// 		}
+	// 		// read result
+	// 		fmt.Printf("row: %s\n", result.Record().String())
+	// 	}
+	// 	if result.Err() != nil {
+	// 		fmt.Printf("Query error: %s\n", result.Err().Error())
+	// 	}
+	// } else {
+	// 	panic(err)
+	// }
+	// Ensures background processes finishes
+	client.Close()
+}
